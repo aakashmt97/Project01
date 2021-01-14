372d8b5a9c02c9cdacd416db227a5fb0356c0aa8
aakashmt97 <tiwari.aakashm.5496@gmail.com>
2021-01-14 20:55:03 +0530

    Removed the Division module from the program

--- Calculator.java
+++ Calculator.java
@@ -12,7 +12,6 @@ public class Calculator {
 			System.out.println("1. Addition");
 			System.out.println("2. Subtraction");
 			System.out.println("3. Multiplication");
-			System.out.println("4. Division");
 			System.out.println("5. Exit");
 			
 			choice = scan.next().charAt(0);
@@ -35,12 +34,6 @@ public class Calculator {
 						a = scan.nextFloat();
 						b = scan.nextFloat();
 						res = a * b;
-						System.out.println("Result = " + res);
-						break;
-			case '4' :	System.out.println("Enter two numbers : ");
-						a = scan.nextFloat();
-						b = scan.nextFloat();
-						res = a / b;
 						System.out.println("Result = " + res);
 						break;
 			}
